d4170d242c964a6b1a35256c209949b54a02b674
Patrick Bowes <patrick@patricks-mbp.myfiosgateway.com>
2022-05-12 20:54:30 -0400

    Adding Bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,13 @@ function home() {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>REST-Rant</h1>
+                <div>
+                    <img src='/images/chia-fruit-drink.jpg' alt='Chia Fruit Drink' />
+                    <div>
+                        Photo by <a href='https://unsplash.com/@cravethebenefits'>Brenda Godinez</a> on <a href='https://unsplash.com/'> Unsplash </a>
+                    </div>
+                </div>
                 <a href='/places'>
                     <button className='btn-primary'>Places Page</button>
                 </a>
