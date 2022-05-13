b5bfa9613597e0e61cab89142bf1f8a4ad7afcc4
Patrick Bowes <patrick@patricks-mbp.myfiosgateway.com>
2022-05-13 17:58:54 -0400

    Set up places page

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -5,9 +5,13 @@ function index(data) {
     let placesFormatted = data.places.map((place, index) => {
         return (
             <div className='col-sm-6'>
-                <h2>{place.name}</h2>
+                <h2>
+                    <a href={`/places/${index}`}>
+                        {place.name}
+                    </a>
+                </h2>
                 <p className='text-center'>
-                    {place.crusines}
+                    {place.cuisines}
                 </p>
                 <img src={place.pic} alt={place.name} />
                 <p className='text-center'>
