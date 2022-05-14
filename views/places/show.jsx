44dc86c544f4e841fb3564de9dc50506be6bba2a
Patrick Bowes <patrick@patricks-mbp.myfiosgateway.com>
2022-05-14 09:12:13 -0400

    create and use helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,13 +6,18 @@ function show(data) {
         <Def>
             <main>
                 <h1>{data.place.name}</h1>
+                <img src={data.place.pic} alt={data.place.name} />
+                <h3>
+                    Located in {data.place.city}, {data.place.state}
+                </h3>
                 <div>
                     <h2>Rating</h2>
                     <p>Not Rated</p>
                 </div>
                 <div>
                     <h2>Description</h2>
-                    <h4>Located in {data.place.city}, {data.place.state} and serves {data.place.cuisines}</h4>
+                    <h3>{data.place.showEstablished()}</h3>
+                    <h4>Serving {data.place.cuisines}</h4>
                 </div>
                 <br />
                 <div>
